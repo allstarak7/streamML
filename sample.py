22185af6f9b81f427e946d9946373ebd52a040d5
allstarak7 <aditya.kesavan@gmail.com>
2023-10-04 16:59:18 -0400

    data input error

--- sample.py
+++ sample.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Ml4641 Team 5')
+st.header("Written Proposal")
+st.subheader("3 Sources")
+st.header("Contibution Table")
+conSer = pd.Series(["Clean Dataset, Sources","Writing, Sources","Writing","Video, Sources","Video"], index = ["Aditya", "Matthew", "Sohum", "William", "David"])
+conDf = pd.DataFrame(data=conSer, index = ["Aditya", "Matthew", "Sohum", "William", "David"],  columns=["Contributions"])
+st.table(conDf)
+st.header("Dataset")
+data = pd.read_csv("2022-2023 NBA Player Stats - Regular.csv", lineterminator='\n', error_bad_lines=False, encoding='latin-1')
+st.write(data)
+st.header("Checkpoint Definition")
+st.header("Timeline")
 
-streamlit run sample.py
+
+# streamlit run sample.py

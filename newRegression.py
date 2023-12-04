8e9f41f795e3a08d1ffdb0f763cc9a7038577b3d
allstarak7 <aditya.kesavan@gmail.com>
2023-12-04 12:27:42 -0500

    added new data

---
+++ newRegression.py
@@ -0,0 +1,32 @@
+from feature_selection import FeatureSelection
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+class Regression():
+    def linear_regression():
+        # perform the feature selection
+        new_features, new_dataset = FeatureSelection.select_features()
+
+        # perform the regression on the new dataset obtained from feature selection
+
+        # print(new_features)
+        # print(new_dataset)
+
+        # features for up to 2021, every column except last
+        X = new_dataset[new_dataset[:, 0] < 2023][:, :-1]
+        # target variable, last column
+        y = new_dataset[new_dataset[:, 0] < 2023][:, -1]
+
+        X_test = new_dataset[new_dataset[:, 0] == 2022][:, :-1]
+        y_test_actual = new_dataset[new_dataset[:, 0] == 2022][:, -1]
+
+        model = LinearRegression()
+        model.fit(X, y)
+
+        y_test_predicted = model.predict(X_test)
+        print(y_test_predicted)
+        mse = mean_squared_error(y_test_actual, y_test_predicted)
+
+        return y_test_predicted 
